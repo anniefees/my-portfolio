154a02cf9cbac9720b14387053003e9c28a09672
Annie Fees <feesa1115@gmail.com>
2024-09-07 01:16:26 -0400

    update router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
@@ -9,21 +9,19 @@ import Contact from './components/Contact';
 
 function App() {
   return (
-    <Router basename="/my-portfolio"> {/* Use basename for GitHub Pages */}
+    <Router>
       <div className="App">
         <Header />
         <nav>
           <ul>
-            <li><Link to="/">Home</Link></li> {/* Link for client-side routing */}
+            <li><Link to="/">Home</Link></li>
             <li><Link to="/resume">Resume</Link></li>
             <li><Link to="/projects">Projects</Link></li>
             <li><Link to="/contact">Contact</Link></li>
           </ul>
         </nav>
-        
-        {/* Define routes for different pages */}
         <Routes>
-          <Route path="/" element={<Home />} /> {/* Home is the default page */}
+          <Route path="/" element={<Home />} />
           <Route path="/resume" element={<Resume />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
@@ -32,5 +30,6 @@ function App() {
     </Router>
   );
 }
+
 
 export default App;
